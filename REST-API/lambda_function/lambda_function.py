9eaac0bce01949414e244d6818fc213f08a13021
vxidh <vaidheeshbharad@gmail.com>
2024-07-05 11:27:27 +0530

    -

---
+++ REST-API/lambda_function/lambda_function.py
@@ -0,0 +1,84 @@
+import json
+import logging
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+data_store = []
+
+def lambda_handler(event, context):
+    logger.info(f"Received event: {event}")
+    method = event['httpMethod']
+    path_params = event.get('pathParameters', {})
+    body = json.loads(event.get('body', '{}'))
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'Unsupported method'}),
+    }
+
+    if method == 'GET':
+        response = handle_get()
+    elif method == 'POST':
+        response = handle_post(body)
+    elif method == 'PUT':
+        response = handle_put(path_params, body)
+    elif method == 'DELETE':
+        response = handle_delete(path_params)
+    elif method == 'PATCH':
+        response = handle_patch(path_params, body)
+
+    return response
+
+def handle_get():
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data_store)
+    }
+
+def handle_post(body):
+    data_store.append(body)
+    return {
+        'statusCode': 201,
+        'body': json.dumps(body)
+    }
+
+def handle_put(path_params, body):
+    id = int(path_params.get('id', -1))
+    if 0 <= id < len(data_store):
+        data_store[id] = body
+        return {
+            'statusCode': 200,
+            'body': json.dumps(data_store[id])
+        }
+    return {
+        'statusCode': 404,
+        'body': json.dumps({'error': 'Data not found'})
+    }
+
+def handle_delete(path_params):
+    id = int(path_params.get('id', -1))
+    if 0 <= id < len(data_store):
+        deleted_data = data_store.pop(id)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(deleted_data)
+        }
+    return {
+        'statusCode': 404,
+        'body': json.dumps({'error': 'Data not found'})
+    }
+
+def handle_patch(path_params, body):
+    id = int(path_params.get('id', -1))
+    if 0 <= id < len(data_store):
+        data_store[id].update(body)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(data_store[id])
+        }
+    return {
+        'statusCode': 404,
+        'body': json.dumps({'error': 'Data not found'})
+    }
---
+++ REST-API/main.tf
@@ -0,0 +1,83 @@
+resource "aws_iam_role" "lambda_execution" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_execution_policy" {
+  role       = aws_iam_role.lambda_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
+}
+
+data "archive_file" "lambda_function" {
+  type        = "zip"
+  source_dir  = "lambda_function"
+  output_path = "lambda_function.zip"
+}
+
+resource "aws_lambda_function" "api" {
+  filename         = data.archive_file.lambda_function.output_path
+  function_name    = "api_handler"
+  role             = aws_iam_role.lambda_execution.arn
+  handler          = "lambda_function.lambda_handler"
+  source_code_hash = data.archive_file.lambda_function.output_base64sha256
+  runtime          = "python3.8"
+  timeout          = 15
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "LambdaAPI"
+  description = "REST API with Lambda integration"
+}
+
+resource "aws_api_gateway_resource" "api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "data"
+}
+
+resource "aws_api_gateway_method" "api_method" {
+  for_each      = toset(["GET", "POST", "PUT", "DELETE", "PATCH"])
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.api_resource.id
+  http_method   = each.key
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  for_each                = toset(["GET", "POST", "PUT", "DELETE", "PATCH"])
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.api_resource.id
+  http_method             = aws_api_gateway_method.api_method[each.key].http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "v1"
+}
+
+resource "aws_lambda_permission" "api_gateway_permission" {
+  for_each      = toset(["GET", "POST", "PUT", "DELETE", "PATCH"])
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/${each.key}/data"
+}
+
+output "api_url" {
+  value = "${aws_api_gateway_deployment.api_deployment.invoke_url}/v1/data"
+}
